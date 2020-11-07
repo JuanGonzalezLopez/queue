d1307ce41e5b26b842be070626b089e124d984c0
Juanma <juan.gonzalez63@upr.edu>
2020-11-07 19:21:53 -0400

    jk

---
+++ src/edu/uprm/cse/queue/DoublyLinkedQueue.java
@@ -0,0 +1,130 @@
+package edu.uprm.cse.queue;
+
+import java.io.PrintStream;
+
+public class DoublyLinkedQueue<E> implements Queue<E> {
+    private static class Node<E> {
+        private E element; // reference to value stored in the Node
+        private Node<E> next;  // reference to next Node in the chain
+        private Node<E> prev;  // reference to previous Node in the chain
+
+
+        public Node(E element, Node<E> next,Node<E> prev) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        public Node(){
+            this.element = null;
+            this.next = null;
+            this.prev = null;
+
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node<E> prev) {
+            this.prev = prev;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            // this is a  Node
+            ///this.next = next - next of this is the parameter
+            this.next = next;
+        }
+    }
+
+    private Node<E> header;
+    private Node<E> tail;
+    private int currentSize;
+    public DoublyLinkedQueue(){
+        this.currentSize = 0;
+        this.header = new Node<>();
+        this.tail = new Node<>();
+
+        this.header.setNext(this.tail);
+        this.tail.setPrev(this.header);
+
+    }
+
+
+    @Override
+    public int size() {
+        return this.currentSize;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size()==0;
+    }
+
+    @Override
+    public E front() {
+        return (this.isEmpty()? null: this.header.getNext().getElement());
+    }
+
+    @Override
+    public void enqueue(E e) {
+        Node <E> newNode = new Node<>();
+        newNode.setElement(e);
+        newNode.setNext(this.tail);
+        newNode.setPrev(this.tail.getPrev());
+        this.tail.getPrev().setNext(newNode);
+        this.tail.setPrev(newNode);
+        this.currentSize++;
+
+    }
+
+    @Override
+    public E dequeue() {
+        Node<E> temp = null;
+        E result = null;
+        if(this.isEmpty()){
+            return null;
+        }
+        else{
+            temp = header.getNext();
+            result = temp.getElement();
+            temp.getNext().setPrev(this.header);//Step 1
+            this.header.setNext((temp.getNext()));//Step 2
+            temp.setElement(null); //Step 3
+            temp.setNext(null); //Step 4
+            temp.setPrev(null);//Step 5
+            this.currentSize--; //Step 6
+            return result;
+
+        }
+
+    }
+
+    @Override
+    public void clear() {
+        while(this.dequeue()!=null);
+
+    }
+
+    @Override
+    public void print(PrintStream P) {
+        Node<E> temp = this.header.getNext();
+        while(temp!=this.tail){
+            P.println(temp.getElement());
+            temp=temp.getNext();
+        }
+
+    }
+}
---
+++ src/edu/uprm/cse/queue/Queue.java
@@ -0,0 +1,13 @@
+package edu.uprm.cse.queue;
+
+import java.io.PrintStream;
+
+public interface Queue<E> {
+    public int size();
+    public boolean isEmpty();
+    public E front();
+    public void enqueue(E e);
+    public E dequeue();
+    public void clear();
+    public void print(PrintStream P);
+}
